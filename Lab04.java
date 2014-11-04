9918e2d7a4ed1d692f3eb9b90917b4c105090c18
AlexanderFerragamo <alexander_ferragamo@asl.org>
2014-11-04 14:42:59 +0000

    Lab04

--- Lab04.java
+++ Lab04.java
@@ -9,9 +9,84 @@
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
-public class Lab04 {
-
-     public static void main(String[] args) {
-          // TODO Your code goes here
-     }
+public class Lab04
+{
+  public static void takeTheField(Athlete arg)
+  {
+	arg.move();
+	arg.move();
+	arg.move();
+	arg.move();
+	arg.turnRight();
+	arg.move();
+	arg.move();
+  }
+  public static void coach(Athlete arg){
+  arg.move();
+  arg.move();
+  arg.move();
+  arg.move();
+  arg.move();
+  arg.move();
+  arg.turnRight();
+  arg.move();
 }
+  
+  public static void main(String[] args) {
+          
+             Display.openWorld("maps/arena.map");
+   Display.setSize(10, 10);
+   Display.setSpeed(5);
+
+   Athlete pete = new Athlete();
+   Athlete john = new Athlete();
+   Athlete jack = new Athlete();
+   Athlete pat = new Athlete();
+   Athlete alex = new Athlete();
+   Athlete joe = new Athlete();
+   Athlete bob = new Athlete();
+   
+   takeTheField(pete);
+	takeTheField(john);
+   takeTheField(jack);
+   takeTheField(pat);
+   takeTheField(alex);
+   takeTheField(joe);
+   pete.move();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnAround();
+   john.move();
+   john.move();
+   john.move();
+   john.move();
+   john.move();
+   john.turnLeft();
+   john.move();
+   john.turnAround();
+   jack.move();
+   jack.move();
+   jack.move();
+   jack.move();
+   jack.turnRight();
+   pat.move();
+   pat.move();
+   pat.move();
+   pat.turnRight();
+   alex.move();
+   alex.move();
+   alex.turnRight();
+   joe.move();
+   joe.turnLeft();
+   joe.move();
+   joe.turnAround();
+   coach(bob);
+   
+
+   }
+   
+}
+
